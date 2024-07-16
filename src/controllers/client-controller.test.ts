ae7e8154f10b87edeffc8953ecff9d2bb6366e02
tj <71949155+tjbarbosa@users.noreply.github.com>
2024-07-16 11:58:15 -0300

    test: corrige testes do controller

--- src/controllers/client-controller.test.ts
+++ src/controllers/client-controller.test.ts
@@ -2,7 +2,13 @@ import {Request, Response} from 'express';
 import {checkEligibility} from '../controllers/client-controller';
 import {ClientService} from '../services/client-service';
 import {ClientPresenter} from '../presenters/client-presenter';
-import { ValidConsumptionClassEnum, ValidTariffModalityEnum, ConnectionTypeEnum } from '../types/client.type';
+import {
+  ValidConsumptionClassEnum,
+  ValidTariffModalityEnum,
+  ConnectionTypeEnum,
+  InvalidConsumptionClassEnum,
+  InvalidTariffModalityEnum,
+} from '../types/client.type';
 
 jest.mock('../services/client-service');
 jest.mock('../presenters/client-presenter');
@@ -32,51 +38,80 @@ describe('Client Controller - checkEligibility', () => {
   });
 
   it('should return 200 if eligible', () => {
-    const input = {
-      numeroDoDocumento: '14041737706',
-      tipoDeConexao: 'bifasico',
-      classeDeConsumo: 'comercial',
-      modalidadeTarifaria: 'convencional',
-      historicoDeConsumo: [3878, 9760, 5976, 2797, 2481, 5731, 7538, 4392, 7859, 4160, 6941, 4597],
-    };
-
-    const inputService = {
+    const serviceInput = {
       consumptionClass: ValidConsumptionClassEnum.commercial,
       tariffModality: ValidTariffModalityEnum.conventional,
       connectionType: ConnectionTypeEnum.twoPhase,
       consumptionHistory: [3878, 9760, 5976, 2797, 2481, 5731, 7538, 4392, 7859, 4160, 6941, 4597],
     };
 
+    const serviceOutput = {
+      eligible: true,
+      annualCO2Savings: 123,
+    };
 
-
-    const mockResult = {
+    const presenterOutput = {
       elegivel: true,
       economiaAnualDeCO2: 123,
     };
-    mockCheckEligibility.mockReturnValue({
-      eligible: true,
-      annualCO2Savings: 123,
-    });
-    mockBindToPortuguese.mockReturnValue(mockResult);
+
+    mockCheckEligibility.mockReturnValue(serviceOutput);
+    mockBindToPortuguese.mockReturnValue(presenterOutput);
 
     checkEligibility(req as Request, res as Response);
 
-    expect(mockCheckEligibility).toHaveBeenCalledWith(inputService);
-    expect(mockBindToPortuguese).toHaveBeenCalledWith(inputService);
+    expect(mockCheckEligibility).toHaveBeenCalledWith(serviceInput);
+    expect(mockBindToPortuguese).toHaveBeenCalledWith(serviceOutput);
     expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith(mockResult);
+    expect(res.json).toHaveBeenCalledWith(presenterOutput);
   });
 
   it('should return 422 if not eligible', () => {
-    const mockResult = {elegivel: false};
-    mockCheckEligibility.mockReturnValue(mockResult);
-    mockBindToPortuguese.mockReturnValue(mockResult);
+    req = {
+      body: {
+        numeroDoDocumento: '14041737706',
+        tipoDeConexao: 'monofasico',
+        classeDeConsumo: 'poderPublico',
+        modalidadeTarifaria: 'azul',
+        historicoDeConsumo: [78, 60, 76, 97, 81, 31, 38, 92, 59, 60, 41, 97],
+      },
+    };
+
+    const serviceInput = {
+      consumptionClass: InvalidConsumptionClassEnum.publicPower,
+      tariffModality: InvalidTariffModalityEnum.blue,
+      connectionType: ConnectionTypeEnum.singlePhase,
+      consumptionHistory: [78, 60, 76, 97, 81, 31, 38, 92, 59, 60, 41, 97],
+    };
+
+    const serviceOutput = {
+      eligible: false,
+      reasons: [
+        'Classe de consumo não elegível',
+        'Modalidade tarifária não elegível',
+        'Histórico de consumo insuficiente',
+        'Consumo médio abaixo do mínimo necessário',
+      ],
+    };
+
+    const presenterOutput = {
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        'Classe de consumo não elegível',
+        'Modalidade tarifária não elegível',
+        'Histórico de consumo insuficiente',
+        'Consumo médio abaixo do mínimo necessário',
+      ],
+    };
+
+    mockCheckEligibility.mockReturnValue(serviceOutput);
+    mockBindToPortuguese.mockReturnValue(presenterOutput);
 
     checkEligibility(req as Request, res as Response);
 
-    expect(mockCheckEligibility).toHaveBeenCalledWith(req.body);
-    expect(mockBindToPortuguese).toHaveBeenCalledWith(mockResult);
+    expect(mockCheckEligibility).toHaveBeenCalledWith(serviceInput);
+    expect(mockBindToPortuguese).toHaveBeenCalledWith(serviceOutput);
     expect(res.status).toHaveBeenCalledWith(422);
-    expect(res.json).toHaveBeenCalledWith(mockResult);
+    expect(res.json).toHaveBeenCalledWith(presenterOutput);
   });
 });
